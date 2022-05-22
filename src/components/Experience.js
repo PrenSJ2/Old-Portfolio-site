66879b637a9fc4e8999956efc1e6f0d476a8fb43
PrenSJ2 <seb.prentice@hotmail.com>
2022-05-22 17:18:00 +0100

    first commit

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -37,7 +37,7 @@ class Experience extends Component {
               color: "#fff",
               textAlign: "center",
             }}
-            icon={<i className="fab fa-angular experience-icon"></i>}
+            icon={<i className="fas fa-briefcase experience-icon"></i>}
             key={i}
           >
             <div style={{ textAlign: "left", marginBottom: "4px" }}>
@@ -56,6 +56,9 @@ class Experience extends Component {
             >
               {work.company}
             </h4>
+            <p style={{ textAlign: "left" }}>
+              {work.description}
+            </p>
             <div style={{ textAlign: "left", marginTop: "15px" }}>{tech}</div>
           </VerticalTimelineElement>
         );
